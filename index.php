e120e06de5a935473c1f38fffac7d7369a4078d7
xkas01 <xkas2001@gmail.com>
2022-09-29 23:29:10 +0500

    test insert function and getResult

--- index.php
+++ index.php
@@ -3,3 +3,7 @@
 include 'Database.php';
 
 $obj = new Database();
+
+$obj->insert('students', ['student_name' => 'Soliy', 'age' => '20', 'city' => 'Gurlan']);
+echo "Insert result is : ";
+print_r($obj->getResult());
