5c866a4a0331ac1cabbf5b010d9662c21dfccb95
xkas01 <xkas2001@gmail.com>
2022-09-29 22:44:05 +0500

    write DB details

--- Database.php
+++ Database.php
@@ -2,5 +2,44 @@
 
 class Database
 {
+    private $db_host = "localhost";
+    private $db_user = "ata";
+    private $db_pass = "123456";
+    private $db_name = "testing";
 
+    private $conn = false;
+
+    public function __construct()
+    {
+    }
+
+    // Insert Function
+    public function insert()
+    {
+
+    }
+
+    // Update Function
+    public function update()
+    {
+
+    }
+
+    // Delete Function
+    public function delete()
+    {
+
+    }
+
+    // Select Function
+    public function select()
+    {
+
+    }
+
+    //close connection
+    public function __destruct()
+    {
+
+    }
 }
